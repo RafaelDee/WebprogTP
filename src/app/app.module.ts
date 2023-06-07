5264f98aed32a76e00ede1eda40afdd3e2a27884
Rafael Dee <dryestboot@gmail.com>
2023-06-08 01:06:34 +0800

    changed file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Home/home.component';
-import { GameDescriptionComponent } from './GameDescription/game-description.component';
-import { ProfileComponent } from './Profile/profile.component';
-import { AboutComponent } from './About/about.component';
-import { AccountManagerComponent } from './AccountManager/account-manager.component';
-import { GameCodesComponent } from './GameCodes/game-codes.component';
-import { CommunityComponent } from './community/community.component';
+import { HomeComponent } from './home/home.component';
+import { GameDescriptionComponent } from './gameDescription/game-description.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { AccountManagerComponent } from './accountManager/account-manager.component';
+import { GameCodesComponent } from './gameCodes/game-codes.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CommunityComponent } from './community/community.component';
 //import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
