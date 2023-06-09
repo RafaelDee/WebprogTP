2ba07c7ae75dfdc57e9190a01fcebc29eea27760
Rafael Dee <dryestboot@gmail.com>
2023-06-10 00:26:51 +0800

    home update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GameItem } from '../game-item/game-item.component';
 
 @Component({
@@ -8,8 +8,8 @@ import { GameItem } from '../game-item/game-item.component';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  constructor(private modalService: NgbModal) {}
-
+  //constructor(private modalService: NgbModal) {}
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   items:GameItem[] = [
     {cover:'https://cdn.mos.cms.futurecdn.net/JarKa4TVZxSCuN8x8WNPSN.jpg',title:'Genshin Impact',description:"asd"},
     {cover:'https://cdn.mos.cms.futurecdn.net/JarKa4TVZxSCuN8x8WNPSN.jpg',title:'Genshin Impact',description:loremIpsum},
@@ -19,4 +19,3 @@ export class HomeComponent {
     {cover:'https://cdn.mos.cms.futurecdn.net/JarKa4TVZxSCuN8x8WNPSN.jpg',title:'Genshin Impact',description:loremIpsum},
   ];
 }
-export const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
