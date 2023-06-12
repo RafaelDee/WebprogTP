e5970203bd9318709f5c813fa63de794c1f45e17
Rafael Dee <dryestboot@gmail.com>
2023-06-12 21:12:47 +0800

    updated ui

--- src/app/GameDescription/game-description.component.ts
+++ src/app/GameDescription/game-description.component.ts
@@ -5,4 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './game-description.component.html',
   styleUrls: ['./game-description.component.scss']
 })
-export class GameDescriptionComponent {}
+export class GameDescriptionComponent {
+  currentRate = 3.5
+  ariaValueText(current: number, max: number) {
+		return `${current} out of ${max} hearts`;
+	}
+}
+function getFileType(file) {
+
+  if(file.type.match('image.*'))
+    return 'image';
+
+  if(file.type.match('video.*'))
+    return 'video';
+
+  if(file.type.match('audio.*'))
+    return 'audio';
+
+  // etc...
+
+  return 'other';
+}
+class Game{
+  title:string;
+  covers:string[];
+  titleCover:string;
+  background?:string[];
+  shortDesc:string;
+  description:string;
+  specification:{recommended:{[key: string]: string}[],minimum:{[key: string]: string}[]}
+  supportedLang:{[key: string]: string}[];
+}
