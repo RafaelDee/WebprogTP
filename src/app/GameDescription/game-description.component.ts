8783df6471c3fa0a1cbd67211a4533a2c8984971
Rafael Dee <dryestboot@gmail.com>
2023-06-14 01:34:00 +0800

    final home and gamedescription

--- src/app/GameDescription/game-description.component.ts
+++ src/app/GameDescription/game-description.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { games } from 'src/assets/database';
 import * as moment from 'moment';
 import { ActivatedRoute } from '@angular/router';
-import * as bootstrap from 'bootstrap';
-
 @Component({
   selector: 'app-game-description',
   templateUrl: './game-description.component.html',
@@ -11,7 +9,6 @@ import * as bootstrap from 'bootstrap';
 })
 export class GameDescriptionComponent {
   moment = moment;
-  carousel:bootstrap.Carousel;
   currentGame;
   playingIndex:number = null;
   constructor(private route: ActivatedRoute) {}
@@ -19,13 +16,11 @@ export class GameDescriptionComponent {
     return `${current} out of ${max} hearts`;
   }
   ngAfterViewInit(){
-    this.carousel = new bootstrap.Carousel(document.querySelector('#gamesCarousel'))
     const myCarousel = document.querySelector('#gamesCarousel');
     myCarousel.querySelectorAll('video').forEach((element) => {
       element.volume = 0.3;
     });
     myCarousel.querySelectorAll('video')[0]?.play();
-    this.carousel.pause();
   }
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get('id');
@@ -35,14 +30,12 @@ export class GameDescriptionComponent {
       if(this.playingIndex != null){
         console.log('stopping');
         (event as unknown as Event).stopPropagation();
-        this.carousel.pause();
       }
       console.log('asd');
       myCarousel.querySelectorAll('video').forEach((element) => {
         element.pause();
       });
     });
-    this.carousel?.pause();
   }
   playing(index){
     this.playingIndex = index;
@@ -50,10 +43,8 @@ export class GameDescriptionComponent {
   pause() {
     this.playingIndex = null;
     console.log('pause')
-    this.carousel.cycle();
   }
   play() {
     console.log('play')
-    this.carousel.pause();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
+
 /// <reference types="@angular/localize" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
 export const appname = "Pixel Thrift"
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
 document.title = appname
