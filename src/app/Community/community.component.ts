841334691b71e470ce5cb9306dc4be8f1a55f1b6
Rafael Dee <dryestboot@gmail.com>
2023-06-22 23:05:42 +0800

    community done

--- src/app/Community/community.component.ts
+++ src/app/Community/community.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-community',
   templateUrl: './community.component.html',
   styleUrls: ['./community.component.css']
 })
-export class CommunityComponent {}
+export class CommunityComponent {
+  createTopics(): Topic {
+    return {
+      title: faker.hacker.adjective(),
+      image: faker.image.urlLoremFlickr(),
+      content: faker.lorem.sentences(3),
+    };
+  }
+  topics:Topic[] = faker.helpers.multiple(
+    this.createTopics,
+    {
+      count: 15,
+    }
+  );
+}
+export interface Topic{
+  title:string;
+  image:string;
+  content:string;
+}
